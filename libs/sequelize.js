eae0184a5b3758f4969858bb891e436fd7b934ba
Yarilenis Laverde <yarilaverde@gmail.com>
2022-08-11 19:11:35 -0500

    one to one relationships

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -4,18 +4,17 @@ const setupModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-/* const sequelize = new Sequelize(URI, {
-    // dialect: 'postgres',
-    dialect: 'mysql',
-    // logging: true,
-}); */
+const sequelize = new Sequelize(URI, {
+    dialect: 'postgres',
+    logging: true,
+});
 
-const sequelize = new Sequelize(config.dbName, USER, PASSWORD, {
+/* const sequelize = new Sequelize(config.dbName, USER, PASSWORD, {
     host: config.dbHost,
     dialect: 'mysql',
-});
+}); */
 
 setupModels(sequelize);
 
--- services/category.service.js
+++ services/category.service.js
@@ -1,32 +1,67 @@
-const boom = require('@hapi/boom');
-
+const faker = require('faker');
 class CategoryService {
 
-  constructor(){
+  constructor() {
+    this.categories = [];
+    this.generate();
   }
+
+  generate() {
+    const limit = 5;
+    for (let index = 0; index < limit; index++) {
+      this.categories.push({
+        id: faker.datatype.uuid(),
+        name: faker.commerce.department()
+      });
+    }
+  }
+
   async create(data) {
-    return data;
+    const newCategory = {
+      id: faker.datatype.uuid(),
+      ...data
+    }
+    this.categories.push(newCategory);
+    return newCategory;
   }
 
   async find() {
-    return [];
+    return this.categories;
   }
 
   async findOne(id) {
-    return { id };
+    return this.categories.find(item => item.id === id);
   }
 
-  async update(id, changes) {
+  filter(name) {
+    return this.categories.filter(item => item.name.includes(name));
+  }
+
+  update(id, changes) {
+    const index = this.categories.findIndex(item => item.id === id);
+    if (index !== -1) {
+      throw new Error('Product no found');
+    }
+
+    const product = this.categories[index];
+    this.categories[index] = {
+      ...product,
+      ...changes
+    };
+    return this.categories[index];
+  }
+
+  delete(id) {
+    const index = this.categories.findIndex(item => item.id === id);
+    if (index !== -1) {
+      throw new Error('Product no found');
+    }
+    this.categories.splice(index, 1);
     return {
       id,
-      changes,
-    };
+      message: 'deleted'
+    }
   }
-
-  async delete(id) {
-    return { id };
-  }
-
 }
 
 module.exports = CategoryService;
