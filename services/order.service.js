6bce478adec66712ea54c0c8d2a49730f2c75d29
Yarilenis Laverde <yarilaverde@gmail.com>
2022-08-12 18:10:52 -0500

    many to many relationship

--- services/order.service.js
+++ services/order.service.js
@@ -1,32 +1,50 @@
 const boom = require('@hapi/boom');
-
+const { models } = require('../libs/sequelize');
 class OrderService {
 
-  constructor(){
-  }
+  constructor() {}
+
   async create(data) {
-    return data;
+    const newOrder = await models.Order.create(data);
+    return newOrder;
+  }
+  async addItem(data) {
+    const newItem = await models.OrderProduct.create(data);
+    return newItem;
   }
 
   async find() {
-    return [];
+    const orders = await models.Order.findAll();
+    return orders;
   }
 
   async findOne(id) {
-    return { id };
+    const order = await models.Order.findByPk(id, {
+      include: [
+        {
+          association: 'customer',
+          include: ['user']
+        },
+        'items'
+      ]
+    });
+    if (!order) {
+      throw boom.notFound('Order not found');
+    }
+    return order;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const model = await this.findOne(id);
+    const order = await model.update(changes);
+    return order;
   }
 
   async delete(id) {
-    return { id };
+    const model = await this.findOne(id);
+    await model.destroy();
+    return { rta: true };
   }
-
 }
 
 module.exports = OrderService;
