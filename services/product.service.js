161cd92a34db30001213c219c4db44079e310362
Yarilenis Laverde <yarilaverde@gmail.com>
2022-08-13 16:41:04 -0500

    filters

--- services/product.service.js
+++ services/product.service.js
@@ -1,5 +1,5 @@
 const boom = require('@hapi/boom');
-
+const { Op } = require('sequelize');
 const { models } = require('../libs/sequelize');
 
 class ProductsService {
@@ -11,21 +11,37 @@ class ProductsService {
     return newProduct;
   }
 
-  async find() {
-    const products = await models.Product.findAll({
-      include: ['category']
-    });
+  async find(query) {
+    const { limit, offset, price, price_min, price_max } = query;
+    const options = {
+      include: ['category'],
+      where: {}
+    };
+    if(limit && offset) {
+      options.limit = limit;
+      options.offset = offset;
+    }
+    if(price)
+      options.where.price = price;
+
+    if(price_min && price_max) {
+      options.where.price = {
+        [Op.gte]: price_min,
+        [Op.lte]: price_max
+      };
+    }
+    const products = await models.Product.findAll(options);
     return products;
   }
 
   async findOne(id) {
     const product = await models.Product.findByPk(id);
-    if (!product) {
+    if (!product)
       throw boom.notFound('Product not found');
-    }
-    if (product.isBlock) {
+
+    if (product.isBlock)
       throw boom.conflict('Product is block');
-    }
+
     return product;
   }
 
