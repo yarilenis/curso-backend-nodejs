25bb2a360b50fc06fe702d3169ea1d13ece90e88
Yarilenis Laverde <yarilaverde@gmail.com>
2022-08-13 17:36:05 -0500

    config database production

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,10 +1,15 @@
 const { Pool } = require('pg');
 
 const { config } = require('./../config/config');
+let URI = '';
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+if (config.isProd) {
+  URI = config.dbUrl;
+} else {
+  const USER = encodeURIComponent(config.dbUser);
+  const PASSWORD = encodeURIComponent(config.dbPassword);
+  URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+}
 
 const pool = new Pool({ connectionString: URI });
 
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -2,22 +2,18 @@ const { Sequelize } = require('sequelize');
 const { config } = require('./../config/config');
 const setupModels = require('./../db/models');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const options = {
+  dialect: 'postgres',
+  logging: config.isProd ? false : true
+}
+if (config.isProd) {
+  options.ssl = {
+    rejectUnauthorized: false
+  }
+}
 
-const sequelize = new Sequelize(URI, {
-    dialect: 'postgres',
-    logging: true,
-});
-
-/* const sequelize = new Sequelize(config.dbName, USER, PASSWORD, {
-    host: config.dbHost,
-    dialect: 'mysql',
-}); */
+const sequelize = new Sequelize(config.dbUrl, options);
 
 setupModels(sequelize);
-
-// sequelize.sync();
 
 module.exports = sequelize;
